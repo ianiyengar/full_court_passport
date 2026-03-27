37f28598ddc3679f542fde939b6e1a7a13434f40
Ian Iyengar <1984therocker@gmail.com>
2026-03-27 15:21:00 -0400

    Add image and stylized card format

--- app/helpers/arenas_helper.rb
+++ app/helpers/arenas_helper.rb
@@ -1,2 +1,38 @@
 module ArenasHelper
+  def arena_image_for(arena)
+    filenames = {
+      "American Airlines Center" => "american-airlines-center.avif",
+      "Ball Arena" => "ball-arena.webp",
+      "Barclays Center" => "barclays-center.avif",
+      "Capital One Arena" => "capital-one-arena.webp",
+      "Chase Center" => "chase-center.webp",
+      "Crypto.com Arena" => "crypto-com-arena.avif",
+      "Delta Center" => "delta-center.avif",
+      "FedExForum" => "fedex-forum.jpg",
+      "Fiserv Forum" => "fiserv-forum.webp",
+      "Footprint Center" => "footprint-center.avif",
+      "Frost Bank Center" => "frost-bank-center.webp",
+      "Gainbridge Fieldhouse" => "gainbridge-fieldhouse.avif",
+      "Golden 1 Center" => "golden1-center.webp",
+      "Intuit Dome" => "intuit-dome.jpeg",
+      "Kaseya Center" => "kaseya-center.avif",
+      "Kia Center" => "kia-center.avif",
+      "Little Caesars Arena" => "little-caesars-arena.avif",
+      "Madison Square Garden" => "madison-square-garden.avif",
+      "Moda Center" => "moda-center.avif",
+      "Paycom Center" => "paycom-center.avif",
+      "Rocket Mortgage FieldHouse" => "rocket-mortgage-fieldhouse.webp",
+      "Scotiabank Arena" => "scotiabank-arena.avif",
+      "Smoothie King Center" => "smoothie-king-center.jpg",
+      "Spectrum Center" => "spectrum-center.webp",
+      "State Farm Arena" => "state-farm-arena.avif",
+      "Target Center" => "target-center.avif",
+      "TD Garden" => "td-garden.avif",
+      "Toyota Center" => "toyota-center.webp",
+      "United Center" => "united-center.jpeg",
+      "Wells Fargo Center" => "wells-fargo-center.avif"
+    }
+
+    filenames[arena.name]
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2026_03_26_195207) do
+ActiveRecord::Schema[7.2].define(version: 2026_03_27_150451) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -27,6 +27,7 @@ ActiveRecord::Schema[7.2].define(version: 2026_03_26_195207) do
     t.decimal "longitude"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_url"
     t.index ["team_id"], name: "index_arenas_on_team_id"
   end
 
